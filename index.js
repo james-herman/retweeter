eff94fdccf7dda01448b84bd4ba53c858e43d86e
james-herman <herman.james@gmail.com>
2019-04-04 22:41:17 -0500

    Filtered out unrelated bot tweets

--- index.js
+++ index.js
@@ -1,49 +1,85 @@
 
 var Twit = require('twit');
-//auth keys held in config
+
+// auth keys held in config
 var config = require('./config')
+
 // new Twit object
 var T = new Twit(config);
 
-//params for searching for tweets, in this case return 2 tweets with the word steelydan
+// params for searching for tweets, in this case return 50 tweets with the word steelydan
 var params = {
 	q: 'steelydan',
-	count: 2
+	count: 50
 }
 
-//function to get tweets
-
+// function to get tweets
 function getTheTweets() {
 	T.get('search/tweets', params, gotData);
 }
+
 // call function
 getTheTweets();
 
 
-// function called when tweets object is returned from Twitter
+// function called when tweets JSON is returned from Twitter
 
 function gotData(err, data, response) {
+
+	// used to pick random tweet of the batch of 50 tweets returned
+	var randomtweet = Math.floor(Math.random() * 50);
+	console.log(randomtweet);
 	// create empty object to hold returned tweet id_str 
 	 var toretweet = {
 	 	id: '',
 	}
+	// variable to hold twitter user id of selected tweet for retweeting
+	var tweeterid = data.statuses[randomtweet].user.id_str;
+
+	// console.log(tweeterid);
+	
+	// grabbing selected tweet id and assigning it to toretweet object
+	toretweet.id = data.statuses[randomtweet].id_str;
+
+	// console.log(toretweet.id);
+	
 	// assign value to tweet using id_str property
 	// using id_str and not id because Javascript can't deal with large integers
 	// so the Twitter api passes them as strings
-	toretweet.id = data.statuses[0].id_str;
+	
+	
+	
+	// add if statement to filter out anime SteelyDan character bots
+	// if the tweet is by either bot there is a message in the console
+	// and in 20 seconds the function will run again, hopefully posting
+	if (tweeterid == ('2470654214' || '122217337')) {
+		console.log("oops its the wrong guy again, will try again");
+		
+		// logging to show info being used
+		// console.log(toretweet.id);
+		// console.log(data.statuses[randomtweet].user.id_str);
+		// console.log(randomtweet);
+	
+		} else {
 
-	// console.log(data.statuses[0].text);
+		// logging to show info being used  
+		// console.log(toretweet.id);
+		// console.log(data.statuses[randomtweet].user.id_str);
+		// console.log(randomtweet);
 
-	// retweet first tweet from search above 
-	// print returned data in console
-	T.post('statuses/retweet/:id', toretweet, function (err, data, response) {
-  		console.log(data);
-	});
+		// retweet tweet from search above 
+		// print returned data in console
+		T.post('statuses/retweet/:id', toretweet, function (err, data, response) {
+   		console.log(data);
+	 	});
+		console.log("Success, another quality Steely Dan tweet has posted!");
+
+	}
 }
 
-// set interval to run function and retweet every 60 seconds
+// set interval to run function and retweet every 20 seconds
 
-setInterval(getTheTweets, 60000);
+setInterval(getTheTweets, 20000);
 
 
 
